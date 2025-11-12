43e3e368934749e7d19bcbb2d64d2191f39b476c
emirPuskaITAkademija <emir.puska@it-akademija.com>
2025-11-12 19:16:40 +0100

    Dodan ThreeDemo i izmijenjen Program.java

--- src/main/java/com/itakademija/Program.java
+++ src/main/java/com/itakademija/Program.java
@@ -2,6 +2,6 @@ package com.itakademija;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Zdravo raja. Kako ste mi danas.");
+        System.out.println("Zdravo raja. Kako ste mi danas. Nadam se da ste ovo razumjeli");
     }
 }
